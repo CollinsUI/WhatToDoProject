b98d85806e0174d43f2bb5ddc3365aa3988d628d
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-12-27 09:23:21 +0300

    Initial Commit

---
+++ WhatToDoProject/WhatToDoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToDoProjectApp.swift
+//  WhatToDoProject
+//
+//  Created by Collins Muthomi on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatToDoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
